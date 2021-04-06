4e4cc06716881d3c6c1b49f44f3f684be084ec44
Viktor Rudenko <gogiandr@gmail.com>
2021-04-06 17:33:08 +0300

    Added methtods POST, PUT, DELETE

--- modules/user/user.controller.js
+++ modules/user/user.controller.js
@@ -22,7 +22,7 @@ router.get("/", function (req, res) {
 //получение конкретного пользователя
 router.get("/:id", function (req, res) {
     const id = req.params.id;
-    let userById = null;
+    let userById;
 
     fs.readFile(filePath, function (err, content) {
         if (err) {
@@ -49,18 +49,157 @@ router.get("/:id", function (req, res) {
 });
 
 //создание пользователя
-router.post("/", function (req, res) {
-    res.send("post");
+router.post("/", jsonParser, function (req, res) {
+    if (!req.body) {
+        res.statusCode = 400;
+        res.end("Bad Request");
+    }
+
+    const userName = req.body.name;
+    const userSurname = req.body.surname;
+    const userEmail = req.body.email;
+    const userPhone = req.body.phone;
+    let userNewId;
+
+    fs.readFile(filePath, function (err, content) {
+        if (err) {
+            res.statusCode = 500;
+            res.end("Server error");
+            return;
+        }
+
+        let users = JSON.parse(content);
+        let maxId = Math.max(...users.map((user) => user.id));
+        userNewId = maxId + 1;
+
+        let newUser = {
+            id: userNewId,
+            name: userName,
+            surname: userSurname,
+            email: userEmail,
+            phone: userPhone,
+        };
+
+        users.push(newUser);
+        data = JSON.stringify(users, null, "\t");
+
+        fs.writeFile("./database/users.json", data, function (err, content) {
+            if (err) {
+                res.statusCode = 500;
+                res.end("Server error");
+                return;
+            }
+
+            res.send(newUser);
+        });
+    });
 });
 
 //обновление пользователя
-router.put("/", function (req, res) {
-    res.send("put");
+router.put("/:id", jsonParser, function (req, res) {
+    const id = req.params.id;
+    let userIndex;
+
+    if (!req.body) {
+        res.statusCode = 400;
+        res.end("Bad Request");
+    }
+
+    const userName = req.body.name;
+    const userSurname = req.body.surname;
+    const userEmail = req.body.email;
+    const userPhone = req.body.phone;
+
+    fs.readFile(filePath, function (err, content) {
+        if (err) {
+            res.statusCode = 500;
+            res.end("Server error");
+            return;
+        }
+
+        let users = JSON.parse(content);
+
+        for (i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                userIndex = i;
+                break;
+            }
+        }
+
+        if (userIndex) {
+            users[userIndex] = {
+                id: id,
+                name: userName,
+                surname: userSurname,
+                email: userEmail,
+                phone: userPhone,
+            };
+
+            data = JSON.stringify(users, null, "\t");
+
+            fs.writeFile(
+                "./database/users.json",
+                data,
+                function (err, content) {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.end("Server error");
+                        return;
+                    }
+
+                    res.send(users[userNumInArr]);
+                }
+            );
+        } else {
+            res.statusCode = 404;
+            res.end("User not found");
+        }
+    });
 });
 
 //удаление пользователя
-router.delete("/", function (req, res) {
-    res.send("delete");
+router.delete("/:id", function (req, res) {
+    const id = req.params.id;
+    let userIndex = -1;
+
+    fs.readFile(filePath, function (err, content) {
+        if (err) {
+            res.statusCode = 500;
+            res.end("Server error");
+            return;
+        }
+
+        let users = JSON.parse(content);
+
+        for (i = 0; i < users.length; i++) {
+            if (users[i].id == id) {
+                userIndex = i;
+                break;
+            }
+        }
+        if (userIndex > -1) {
+            const user = users.splice(userIndex, 1);
+
+            data = JSON.stringify(users, null, "\t");
+
+            fs.writeFile(
+                "./database/users.json",
+                data,
+                function (err, content) {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.end("Server error");
+                        return;
+                    }
+
+                    res.send(user);
+                }
+            );
+        } else {
+            res.statusCode = 404;
+            res.end("User not found");
+        }
+    });
 });
 
 module.exports = router;
